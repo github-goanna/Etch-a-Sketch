08091267b67639a045f51b324392e4132057b404
Jonny Ward <thejonnyward@gmail.com>
2025-03-14 13:11:24 +1100

    Create input button
    Added a button to allow the user to change the size of the grid.
    Functionilised the grid creation into gridCreator
    Added an event listener to the button that asks for a prompt. The event listener then deletes all of the contents within the container and runs gridCreator

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
 const container = document.querySelector(".container")
+const inputButton = document.getElementById("gridSize")
+inputButton.style.width = "300px"
+
 
 let containerDim = 600
-let gridBoxes = 200
-
 container.style.width = containerDim + 'px'
 container.style.height = containerDim + 'px'
 
-let arrayLength = gridBoxes*gridBoxes
+let gridBoxes = 5
 
-for (i=0; i<arrayLength; i++) {
-    gridBox = document.createElement("div")
-    container.appendChild(gridBox)
-    gridBox.setAttribute("class", "box")
-    gridBox.style.width = containerDim/gridBoxes+ 'px';
-    gridBox.style.height = containerDim/gridBoxes +'px';
-    gridFader()
+gridCreator()
+
+function gridCreator() {
+    let arrayLength = gridBoxes * gridBoxes
+
+    for (i = 0; i < arrayLength; i++) {
+        gridBox = document.createElement("div")
+        container.appendChild(gridBox)
+        gridBox.setAttribute("class", "box")
+        gridBox.style.width = containerDim / gridBoxes + 'px';
+        gridBox.style.height = containerDim / gridBoxes + 'px';
+        gridFader()
+    }
 }
 
-function gridFader(){
-        gridBox.addEventListener("mouseover", (event) => {
+function gridFader() {
+    gridBox.addEventListener("mouseover", (event) => {
         event.target.style.backgroundColor = "purple";
         setTimeout(() => {
-            event.target.style.backgroundColor="";
+            event.target.style.backgroundColor = "";
         }, 500);
 
     }, false,)
 }
+
+
+inputButton.addEventListener("click", () => {
+    console.log("hey dere");
+    gridBoxes = prompt("How big do you want the grid to be?", 100);
+    container.textContent = ''
+    gridCreator()
+
+})

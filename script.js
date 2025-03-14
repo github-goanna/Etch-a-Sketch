41ec02b4f37291cdea1e9053892985d1095511d0
Jonny Ward <thejonnyward@gmail.com>
2025-03-14 14:42:11 +1100

    add opacity feature to trailing mouseovers

--- script.js
+++ script.js
@@ -7,19 +7,21 @@ let containerDim = 600
 container.style.width = containerDim + 'px'
 container.style.height = containerDim + 'px'
 
-let gridBoxes = 7
+let gridBoxes = 20
+let gridColour = "green"
 
 gridCreator()
 
 function gridCreator() {
     let arrayLength = gridBoxes * gridBoxes
-
     for (i = 0; i < arrayLength; i++) {
         gridBox = document.createElement("div")
         container.appendChild(gridBox)
         gridBox.setAttribute("class", "box")
         gridBox.style.width = (containerDim / gridBoxes) -0.01 + 'px';
         gridBox.style.height = containerDim / gridBoxes + 'px';
+        gridBox.style.backgroundColor = gridColour;
+        gridBox.style.opacity = 1
         gridFader()
     }
 }
@@ -28,17 +30,15 @@ function gridFader() {
     gridBox.addEventListener("mouseover", (event) => {
         event.target.style.backgroundColor = `rgb(${Math.random()*256},${Math.random()*256},${Math.random()*256})`;
         setTimeout(() => {
-            event.target.style.backgroundColor = "";
+            event.target.style.backgroundColor = gridColour;
         }, 500);
-
+        event.target.style.opacity -= 0.1;
     }, false,)
 }
 
 
 inputButton.addEventListener("click", () => {
-    console.log("hey dere");
     gridBoxes = prompt("How big do you want the grid to be?", 100);
     container.textContent = ''
     gridCreator()
-
 })
